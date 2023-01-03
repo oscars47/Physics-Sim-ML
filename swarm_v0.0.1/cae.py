2bcbaf13092e6afb043cc06927df23d4b8529954
oscars47 <scholinoscar@gmail.com>
2023-01-03 02:59:36 -0800

    updated cae custom

--- swarm_v0.0.1/cae.py
+++ swarm_v0.0.1/cae.py
@@ -2,13 +2,12 @@
 # @oscars47
 
 import os
+import numpy as np
 import tensorflow as tf
 from keras import layers
 from keras.models import Sequential
 from keras.optimizers import Adam
-import wandb
-from wandb.keras import WandbCallback
-from caehelper import *
+from keras.callbacks import ModelCheckpoint
 
 # define img dimesions
 IMG_HEIGHT = 104
@@ -16,7 +15,8 @@ IMG_WIDTH = 104
 input_shape = layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3)) # do care about color
 
 # load data
-DATA_DIR= '/home/oscar47/Desktop/physics/swarm_data/cae_output'
+MAIN_DIR = '/media/oscar47/Oscar Extra/Physics data/swarm_data'
+DATA_DIR= os.path.join(MAIN_DIR, 'cae_output')
 
 train_ds = np.load(os.path.join(DATA_DIR, 'train_ds.npy'))
 val_ds = np.load(os.path.join(DATA_DIR, 'val_ds.npy'))
@@ -68,29 +68,6 @@ def build_cae(input_shape, conv2d1_size=32, conv2d2_size=32, conv2d3_size=32, co
 
     return model
 
-def train(config=None):
-    with wandb.init(config=config):
-    # If called by wandb.agent, as below,
-    # this config will be set by Sweep Controller
-      config = wandb.config
-
-      #pprint.pprint(config)
-
-      #initialize the neural net; 
-      global model
-      model = build_cae(input_shape, conv2d1_size=config.conv2d1_size, conv2d2_size=config.conv2d2_size, conv2d3_size=config.conv2d3_size, convtrans1_size=config.convtrans1_size, 
-         convtrans2_size=config.convtrans2_size, convtrans3_size=config.convtrans1_size, learning_rate = config.learning_rate)
-      
-      #now run training
-      history = model.fit(
-        train_ds, train_ds,
-        batch_size = config.batch_size,
-        validation_data=(val_ds, val_ds),
-        shuffle=False,
-        epochs=config.epochs,
-        callbacks=[WandbCallback()] #use callbacks to have w&b log stats; will automatically save best model                     
-      )
-
 def train_custom():
    global model
    model = build_cae(input_shape)
@@ -104,69 +81,11 @@ def train_custom():
       epochs=5,                    
    )
 
-
-# set dictionary with random search; optimizing val_loss--------------------------
-sweep_config= {
-    'method': 'random',
-    'name': 'val_accuracy',
-    'goal': 'maximize'
-}
-
-sweep_config['metric']= 'val_accuracy'
-
-parameters_dict = {
-    'epochs': {
-       'distribution': 'int_uniform',
-       'min': 15,
-       'max': 20
-    },
-    # for build_dataset
-     'batch_size': {
-       'values': [x for x in range(32, 161, 32)]
-    },
-    'conv2d1_size': {
-       'distribution': 'int_uniform',
-       'min': 64,
-       'max': 256
-    },
-    'conv2d2_size': {
-       'distribution': 'int_uniform',
-       'min': 64,
-       'max': 256
-    },'conv2d3_size': {
-       'distribution': 'int_uniform',
-       'min': 64,
-       'max': 256
-    },'convtrans1_size': {
-       'distribution': 'int_uniform',
-       'min': 64,
-       'max': 256
-    },'convtrans2_size': {
-       'distribution': 'int_uniform',
-       'min': 64,
-       'max': 256
-    },
-    'convtrans3_size': {
-       'distribution': 'int_uniform',
-       'min': 64,
-       'max': 256
-    },
-    'learning_rate':{
-         #uniform distribution between 0 and 1
-         'distribution': 'uniform', 
-         'min': 0,
-         'max': 0.1
-     }
-}
-
-# append parameters to sweep config
-# sweep_config['parameters'] = parameters_dict 
-
-# # login to wandb----------------
-# wandb.init(project="PHLUID-SwarmCAE1", entity="oscarscholin")
-
-# # initialize sweep agent
-# sweep_id = wandb.sweep(sweep_config, project='PHLUID-SwarmCAE1', entity="oscarscholin")
-# wandb.agent(sweep_id, train, count=20)
+if not(os.path.exists(os.path.join(MAIN_DIR, 'models'))):
+    os.mkdir(os.path.join(MAIN_DIR, 'models'))
+modelpath = os.path.join(MAIN_DIR, 'models', 'custom1.hf5')
+checkpoint = ModelCheckpoint(modelpath, monitor='loss',
+                             verbose=1, save_best_only=True,
+                             mode='min')
 
 train_custom()
