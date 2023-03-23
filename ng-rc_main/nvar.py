ab752f06a9916b0c6dd711253063788890fc61ac
Oscar Scholin <orsa2020@mymail.pomona.edu>
2023-03-23 03:11:16 -0700

    issue with i

--- ng-rc_main/nvar.py
+++ ng-rc_main/nvar.py
@@ -12,25 +12,25 @@ s = 1 # number of spacings between steps
 soln = np.load('lorentz_soln.npy')
 x,y,z = soln[0], soln[1], soln[2]
 
+# set number of points to use for training
+split_index=199
+soln_train = soln[:split_index]
+
 # perform check: enough data to generate lin vector?
-if k*s > len(x):# total steps we need to go back is k*s
+if k*s > split_index:# total steps we need to go back is k*s
     raise ValueError('k*s must not exceed number of total time steps.')
 
 # figure out i indices to grab
-i_ls = np.arange(k*s, len(x), k*s)
+i_ls = np.arange(k*s, split_index+1, k*s)
 
 ## build linear and nonlinear vector ##
-lin_vec = []
-nonlin_vec=[]
-total_vec = []
-for i in i_ls:
-    total_i =[]
-    # i_vec = []
-    for var in soln:
-        for j in range(i, i-k*s-1, -s): 
-            total_i.append(var[j])
-    # lin_vec.append(np.array(np.array(i_vec)))
+# helper function to generate the total vec 
+def get_linear(i, var, total_i):
+    for j in range(i, i-k*s-1, -s): 
+        total_i.append(var[j])
+    return total_i
 
+def get_total_i(total_i):
     ## now for nonlinear ##
     coef_comb = list(it.combinations_with_replacement(total_i, N)) # get all unique combinations of coefficients of degree N
     # i_nonlin = [] # vector to hold ith nonlinear component
@@ -39,35 +39,28 @@ for i in i_ls:
         for n in n_tuple:
             product*=n
         total_i.append(product)
-    # nonlin_vec.append(np.array(i_nonlin))
-    total_vec.append(total_i) # concatenate
+    
+    return np.array(total_i)
+
+total_vec_train = []
+for i in i_ls:
+    total_i =[] 
+    for var in soln_train:
+        total_i=get_linear(i, var, total_i)
+    total_vec_train.append(get_total_i(total_i)) # concatenate
 
 
 Y_d = np.array(list(zip(x,y,z)))
-if np.shape(total_vec)[0] < np.shape(Y_d)[0]:
-    diff = np.shape(Y_d)[0] - np.shape(total_vec)[0]
+if np.shape(total_vec_train)[0] < np.shape(Y_d)[0]:
+    diff = np.shape(Y_d)[0] - np.shape(total_vec_train)[0]
     Yd = Y_d[:-diff, :]
 
-## now combine to get total ##
-# lin_vec = np.array(lin_vec)
-# nonlin_vec = np.array(nonlin_vec)
-
-split_index= int(0.2*len(total_vec))
-# lin_train= lin_vec[:split_index, :]
-# nonlin_train= nonlin_vec[:split_index, :]
-
-# top = np.concatenate((lin_train, np.zeros((len(lin_train), len(nonlin_vec[0])))), axis=1)
-# bottom = np.concatenate((np.zeros((len(nonlin_train), len(lin_vec[0]))), nonlin_train), axis=1)
-# total_vec = np.concatenate((top, bottom), axis=0)
-
-# # print(total_vec)
-# print(np.shape(total_vec))
-
-## ridge regression ##
+## ridge regression to solve for minimizing W_out##
 alpha = 1e-3
-Yd_train = np.array(Y_d[:split_index])
-total_vec_train = np.array(total_vec[:split_index])
+Yd_train = Y_d[:split_index]
+total_vec_train = np.array(total_vec_train)
 W_out = total_vec_train.T @ (np.linalg.pinv(total_vec_train @ total_vec_train.T + alpha*np.identity(split_index)))
+print('O_tot', total_vec_train[0])
 print('W_out', np.shape(W_out))
 print('W_out tranpose', np.shape(W_out.T))
 print('Yd_train transpose', np.shape(Yd_train.T))
@@ -75,18 +68,63 @@ print('Yd train', np.shape(Yd_train))
 W_out = Yd_train.T @ W_out.T
 print(W_out)
 print('W_out updated', np.shape(W_out))
-W_tot = Yd_train @ W_out
-print('W tot', W_tot)
-
-## need to implement gradient descent ##
 
 ## now apply training!! ##
+# compute the normalized mean square error
+def compute_nmse(Y_actual, Y_pred):
+    return np.sqrt((Y_actual - Y_pred)**2 / len(Y_actual))
+
+# apply nvar on training data
+def nvar_validate(Y_actual):
+    Y_pred = []
+    i = s*k # initially at min value
+    for Yi in Y_actual:
+        print(np.shape(Yi)) # 3, so good
+        total_i = []
+        total_i = get_linear(i, Yi, total_i)
+        print(np.shape(total_i)) # only 2!
+        print(i)
+        total_i = get_total_i(total_i)
+        print(np.shape(total_i)) # syaing it's 5
+        Yi_next = Yi + (W_out@total_i.T).T# simple Euler like step
+        Y_pred.append(Yi_next)
+        i+=1
+    Y_pred = np.array(Y_pred) # convert back to array
+    nmse = compute_nmse(Y_actual, Y_pred)
+    return Y_pred, nmse
+
+# input >= 1 set of points; predict up to set limit of points
+# Y_in is np.array
+def nvar_test(Y_in, limit):
+    i = s*k
+    while len(Y_in) <= limit: # do for as long as we don't go over the limit
+        # compute O total
+        total_i = []
+        total_i = get_linear(i, Y_in[-1], total_i)
+        total_i = get_total_i(total_i)
+        Yi_next = Y_in[-1] + (W_out@total_i.T).T # use the previous term to calculate the next
+        Y_in = np.append(Y_in, Yi_next)
+        i+=1
+    return Y_in
 
 
+# first compute over training points and compute the normalized root mean square error
+Y_train = np.array(list(zip(x,y,z)))[:split_index]
+Y_train_pred, nmse = nvar_validate(Y_train)
 
+# now test on unseen
+limit = 500 # calculate next group of points
+# if limit < s*k:
+#     raise ValueError('need >= %i points for prediction'%s*k)
 
+Y_in = soln[split_index:split_index+s*k]
+Y_test = nvar_validate(Y_train)
 
+print(Y_test)
 
-# Y_train
-
-
+# now do plot
+# plt.figure(figsize=(10,7))
+# ax = plt.figure().add_subplot(projection='3d')
+# x_0,y_0,z_0= 
+# ax.plot(x, y, z)
+# plt.show()
